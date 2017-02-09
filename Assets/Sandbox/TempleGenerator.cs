b765f63e577ed2cf6d7c016196a674752756b50d
Benjamin Wiberg <benjaminwiberg@gmail.com>
2017-02-08 16:49:21 -0800

    Replace auto-generation of temple with button in Inspector

--- Assets/Sandbox/TempleGenerator.cs
+++ Assets/Sandbox/TempleGenerator.cs
@@ -5,7 +5,7 @@ using Utility;
 using Debug = UnityEngine.Debug;
 
 [ExecuteInEditMode]
-public class EditorAutoTempleGenerationTest : MonoBehaviour {
+public class TempleGenerator : MonoBehaviour {
     [Range(2, 10)] public int TempleLevels = 5;
 
     [Range(0.1f, 10.0f)] public float InnerRadius = 0.25f;
@@ -22,20 +22,13 @@ public class EditorAutoTempleGenerationTest : MonoBehaviour {
 
     public Material WallMaterial;
 
-    private List<GameObject> gameObjectsToDestroy = new List<GameObject>();
-
-    private void OnValidate() {
+    public void GenerateTemple() {
         foreach (Transform child in transform) {
-            UnityEditor.EditorApplication.delayCall += () => { DestroyImmediate(child.gameObject); };
+//            UnityEditor.EditorApplication.delayCall += () => { DestroyImmediate(child.gameObject); };
+            GameObject.DestroyImmediate(child.gameObject);
         }
         transform.DetachChildren();
-//
-//        var array = gameObjectsToDestroy.ToArray();
-//        gameObjectsToDestroy.Clear();
-//        for (int i = 0; i < array.Length; i++) {
-//            DestroyImmediate(array[i]);
-//            array[i] = null;
-//        }
+
 
         var sw = new Stopwatch();
         sw.Start();
---
+++ Assets/Sandbox/TempleGeneratorEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TempleGenerator))]
+public class TempleGeneratorEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var generator = (TempleGenerator) target;
+        if(GUILayout.Button("Generate temple"))
+        {
+            generator.GenerateTemple();
+        }
+    }
+}
