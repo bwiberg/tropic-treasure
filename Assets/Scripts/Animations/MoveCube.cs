4f662a3fdd5a81cfb5cbb14128a17121507a12a7
Damian Mrowca <mrowca@gmx.de>
2017-02-17 21:39:37 -0800

    UI initial commit

---
+++ Assets/Scripts/Animations/MoveCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+	public float animationSpeed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		Animator animator = GetComponent<Animator>();
+		animator.speed = animationSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Animations/Moveteddy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveteddy : MonoBehaviour {
+
+	private int move_forward = 10;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(move_forward > 0)
+		{
+		    transform.position = transform.position + new Vector3(0.0f, 0.05f, 0.0f);
+		    move_forward = move_forward - 1;
+		    if(move_forward == 0)
+		        move_forward = -10;
+		}
+		else 
+		{
+			transform.position = transform.position + new Vector3(-0.0f, -0.05f, -0.0f);
+			move_forward = move_forward + 1;
+		    if(move_forward == 0)
+		        move_forward = 10;
+		}
+	}
+}
