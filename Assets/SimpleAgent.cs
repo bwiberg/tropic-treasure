cd6bbeecbf5800f4663525475a9cdf821b1222af
Benjamin Wiberg <benjaminwiberg@gmail.com>
2017-02-19 01:42:39 -0800

    Setup test-scene with agents moving toward center

---
+++ Assets/SimpleAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleAgent : MonoBehaviour {
+	private NavMeshAgent agent;
+
+	public Transform target;
+
+	// Use this for initialization
+	void Awake () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
