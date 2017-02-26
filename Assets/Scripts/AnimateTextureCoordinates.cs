116509e978ec52a9206fd216de8c92de020dd912
Benjamin Wiberg <benjaminwiberg@gmail.com>
2017-02-25 22:32:50 -0800

    Create island ground terrain and ocean

---
+++ Assets/Scripts/AnimateTextureCoordinates.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateTextureCoordinates : MonoBehaviour {
+	private Mesh mesh;
+	private MeshRenderer renderer;
+
+	public Vector2 AnimationSpeed = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		mesh = GetComponent<MeshFilter>().mesh;
+		renderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var uvs = mesh.uv;
+
+		uvs[0] += AnimationSpeed * Time.deltaTime;
+
+		mesh.uv = uvs;
+
+		renderer.material.SetTextureOffset("_MainTex", AnimationSpeed * Time.time);
+	}
+}
