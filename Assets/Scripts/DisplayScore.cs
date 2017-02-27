e33a0baaa8dd501d45c41041d75b2b3b9042b944
Damian Mrowca <mrowca@gmx.de>
2017-02-27 00:16:09 -0800

    clock added

--- Assets/Scripts/DisplayScore.cs
+++ Assets/Scripts/DisplayScore.cs
@@ -5,25 +5,39 @@ using UnityEngine.UI;
 
 public class DisplayScore : MonoBehaviour {
 
-	private Text scoreText;
-	private float score;
+	public GameObject clock;
+	public float rotationSpeed = 3.0f;
+	public float deltaAngle = 12.0f;  
+
+	private Text timeText;
+	private float continousTime;
+	private float seconds;
+
 
 	// Use this for initialization
 	void Start () {
-		score = 0;
-		scoreText = GetComponent<Text>();
-		UpdateScore(0);
+		continousTime = 120;
+		seconds = continousTime;
+		timeText = GetComponent<Text>();
+		UpdateTime(0);
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		//Remove once we have gamelogic
-		float scoreUpdate = Time.deltaTime;
-		UpdateScore(scoreUpdate);
+		float timeUpdate = Time.deltaTime;
+		UpdateTime(timeUpdate);
 	}
 
-	public void UpdateScore(float newScore) {
-		score += newScore;
-		scoreText.text = "Time: " + Mathf.FloorToInt(score);
+	public void UpdateTime(float newTime) {
+		continousTime -= newTime;
+		if(seconds-continousTime > 1.0f)
+		{
+			seconds -= 1;
+			timeText.text = "" + Mathf.FloorToInt(seconds);
+		}
+		float angle = Mathf.Lerp(deltaAngle*(seconds+1), deltaAngle*(seconds), (seconds-continousTime)*rotationSpeed);
+		clock.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+		transform.localRotation = Quaternion.Euler(0f, 0f, -angle);
 	}
 }
