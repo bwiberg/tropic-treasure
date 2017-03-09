57f51f7a84b22ab8916baeff44dc0581ae20a1bd
Damian Mrowca <mrowca@gmx.de>
2017-03-08 22:56:17 -0800

    initial wind power up commit

---
+++ Assets/TestForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			var rb = GetComponent<Rigidbody>();
+			rb.AddTorque(new Vector3(250.0f, 0.0f, 0.0f));
+		}
+	}
+}
