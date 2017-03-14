7765dc1eb5cb0fad0904c327b176c82bcbe0cc97
Ran Gross <ran.gross@yahoo.com>
2017-03-14 03:36:36 -0700

    Added spikes functionality. Currently relies on mouse input whose code will have to be replaced with the respective touch equivalent. Bugs: - animation and enemy death dont match (needs more syncronization) - duration needs to be set appropriatelly to match difficulty (public   variable in spawnSpikes script)

---
+++ Assets/Scripts/spawnSpikes.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using cakeslice;
+using UnityEngine;
+
+public class spawnSpikes : MonoBehaviour {
+
+    public GameObject spawnObject;
+    public float duration;
+    public bool placeTrap;
+
+    private GameObject spikeField;
+
+	// Use this for initialization
+	void Start () {
+        placeTrap = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*if(Input.touchCount > 0)
+        {
+            RaycastHit hit;
+            var touch = Input.GetTouch(0);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            if (touch.phase == TouchPhase.Began
+                && gameObject.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                spikeField = GameObject.Instantiate(spawnObject, hit.point, Quaternion.identity);
+                spikeField.name = string.Format("spikes_{0}", spikeCount);
+                spikeField.transform.SetParent(gameObject.transform.parent);
+            }
+        }*/
+
+        if (placeTrap)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Input.GetMouseButtonDown(0)
+                && gameObject.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                spikeField = GameObject.Instantiate(spawnObject, hit.point, Quaternion.identity);
+                spikeField.name = string.Format("spikes");
+                spikeField.transform.SetParent(gameObject.transform.parent);
+            }
+            if (Input.GetMouseButton(0)
+                && gameObject.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                spikeField.transform.position = hit.point;
+            }
+            if (Input.GetMouseButtonUp(0)
+                && gameObject.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                spikeField.gameObject.GetComponent<Outline>().enabled = false;
+                Destroy(spikeField.gameObject,duration);
+                placeTrap = false;
+            }
+        }
+    }
+
+    public void setTrap()
+    {
+        placeTrap = true;
+    }
+}
---
+++ Assets/Scripts/spikeTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("agent should die\n");
+        if (other.gameObject.GetComponent<SimpleAgent>())
+        {
+            Animator spikeAnim =  GetComponentInChildren<Animator>();
+            spikeAnim.SetTrigger("trigger");
+            GameObject.Destroy(other.gameObject,1f);
+        }
+    }
+}
